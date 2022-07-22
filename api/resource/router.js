f289cd190aee8d67e6ec7ffcf6076fed82906d96
ashurik522 <ashurik522@gmail.com>
2022-07-22 10:53:08 -0400

    through resources api calls

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,31 @@
 // build your `/api/resources` router here
+const express = require('express')
+const Resources = require('./model')
+
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    Resources.getAll(req.body)
+        .then(resources => {
+            res.json(resources)
+        })
+        .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+    Resources.insert(req.body)
+        .then(resource => {
+            res.status(201).json(resource)
+        })
+        .catch(next)
+})
+
+router.use((err, req, res, next) =>{
+    res.status(err.status || 500).json({
+        message: err.message,
+        stack: err.stack
+    })
+})
+
+
+module.exports = router
