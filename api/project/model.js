7a6018b837d3423e8964dcf38f8424013b4efd31
ashurik522 <ashurik522@gmail.com>
2022-07-22 10:41:48 -0400

    through project api calls

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,37 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig')
+
+
+
+function getAll() {
+    return db('projects')
+        .then(projects =>
+            projects.map(p => ({
+                ...p,
+                project_completed: p.project_completed ? true : false
+            }))
+        )
+        .catch(err => err)
+}
+
+function getById(id) {
+    return db('projects')
+        .where('project_id', id)
+        .first()
+        .then(project => {
+           return {...project, project_completed: project.project_completed ? true : false }
+         })
+        .catch(err => console.log(err)) 
+} 
+
+function insert(project) {
+    return db('projects')
+        .insert(project)
+        .then(id => getById(id[0]))
+}
+
+module.exports = {
+    getAll,
+    getById,
+    insert
+}
--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,38 @@
-// build your `/api/projects` router here
+const express = require('express')
+const Projects = require('./model')
+
+
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    Projects.getAll()
+        .then(project => {
+            res.json(project)
+        })
+        .catch(next)
+})
+
+router.get('/:id', (req, res, next) => {
+    Projects.getById(req.params.id)
+        .then(project => {
+            res.json(project)
+        })
+        .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+    Projects.insert(req.body)
+        .then(project => {
+            res.status(201).json(project)
+        })
+        .catch(next)
+})
+
+router.use((err, req, res, next) =>{
+    res.status(err.status || 500).json({
+        message: err.message,
+        stack: err.stack
+    })
+})
+
+module.exports = router
